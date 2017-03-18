ecdd2f3dc9c66c7646c9adc0f621fb3ede163261
zengzhuoyu <zhuoyuceng@creditease.cn>
2017-03-18 14:48:16 +0800

    3-2 安装前端三扩展包、使用sass文件、加入公共页导航

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -3,8 +3,26 @@
 <head>
 	<meta charset="UTF-8">
 	<title>@yield('title','Laravel rumen') - Laravel 入门教程</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-	@yield('content')
+	<!-- 导航栏 -->
+	<header class="navbar navbar-fixed-top navbar-inverse">
+	  <div class="container">
+	    <div class="col-md-offset-1 col-md-10">
+	      <a href="/" id="logo">Sample App</a>
+	      <nav>
+	        <ul class="nav navbar-nav navbar-right">
+	          <li><a href="/help">帮助</a></li>
+	          <li><a href="#">登录</a></li>
+	        </ul>
+	      </nav>
+	    </div>
+	  </div>
+	</header>
+
+	<div class="container">
+		@yield('content')
+	</div>
 </body>
 </html>
